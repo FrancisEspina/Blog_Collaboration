99e33fdaee8798127ba612aa6decf9f092ef23d2
Gian44 <gdrenomeron@up.edu.ph>
2023-07-05 21:58:56 +0800

    Added comments and security

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
@@ -39,11 +42,11 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.destroy
 
-    redirect_to rooth_path, status: :see_other
+    redirect_to root_path, status: :see_other
   end
 
   private
     def article_params
-      params.require(:article).permit(:title, :body)
+      params.require(:article).permit(:title, :body, :status)
     end
 end
